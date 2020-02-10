1cef20b032b7a6c7081e2dc5238a671af8967cc2
CBTE Account <cbte@ac.sdmesa.net>
2020-02-10 10:32:12 -0800

    code for 1b and 1c

--- Assets/PlayMovement.cs
+++ Assets/PlayMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayMovement : MonoBehaviour
 {
+    public float verSpeed = 20;
+    public float hozSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class PlayMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1);
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // Here is my comment
+        transform.Translate(Vector3.forward * Time.deltaTime * verSpeed * verticalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * hozSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * hozSpeed * horizontalInput);
     }
 }
